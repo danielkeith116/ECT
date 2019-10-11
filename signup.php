f6253d661571d4b08e9e9fd3de48e3cb5eee14a6
danielkeith116 <danielkeith116@users.noreply.github.com>
2019-10-11 19:36:56 -0400

    form to php:

---
+++ signup.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<?php echo $_POST["fName"]; ?><br>
+
+<?php echo $_POST["lName"]; ?><br>
+<?php echo $_POST["email"]; ?><br>
+<?php echo $_POST["password"]; ?><br>
+<?php echo $_POST["address"]; ?><br>
+<?php echo $_POST["city"]; ?><br>
+<?php echo $_POST["state"]; ?><br>
+<?php echo $_POST["zipcode"]; ?><br>
+<?php echo $_POST["month"]; ?><br>
+<?php echo $_POST["day"]; ?><br>
+<?php echo $_POST["year"]; ?><br>
+
+
+</body>
+</html>
