8feb65979c3b2821e129a3c54c8a859bc53a2c13
Caroline Ehler <cee7zm@virginia.edu>
2019-10-23 21:51:42 -0400

    emails can send now!

--- contactus.php
+++ contactus.php
@@ -4,6 +4,9 @@
 //use PHPMailer\PHPMailer\PHPMailer;
 //use PHPMailer\PHPMailer\Exception;
 //require 'vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+require 'vendor/autoload.php';
 
 $name = $_POST["inputName"];
 $email = $_POST["email"];
@@ -24,7 +27,25 @@ if(!$mail->send()) {
   echo 'Message has been sent.';
 }*/
 
-mail("emily.mussey@gmail.com", $name, $message);
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->SMTPAuth = true;
+$mail->setFrom('thePersistentCookies@gmail.com', 'Crazy Cookies');
+$mail->Username = 'thePersistentCookies@gmail.com';
+$mail->Password = 'Uvaistheb3st!';
+$mail->addAddress('thePersistentCookies@gmail.com', 'Crazy Cookies');
+$mail->Subject  = "New Contact Us Entry";
+$mail->Body     = 'Name: ' . $name . ' Email: ' . $email . ' Message: ' . $message;
+if(!$mail->send()) {
+  echo 'Message was not sent.';
+  echo 'Mailer error: ' . $mail->ErrorInfo;
+} else {
+  header('Location: /ECT/contactus.html');
+}
 ?>
 </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,6 +1,12 @@
 <html>
 <body>
 <?php
+//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\Exception;
+//require 'vendor/autoload.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+require 'vendor/autoload.php';
 $db_c = pg_connect("host=ec2-174-129-218-200.compute-1.amazonaws.com	 port = 5432 dbname=d1taisgtq9v2u6 user=lwaasictcokjps 
 password=7cc9f025cacf299ba7640082a90881e65a38903fc048e3dd326700b7ebf3db8f");
 $fn = $_POST["fName"];
@@ -15,22 +21,21 @@ $bm = $_POST["month"];
 $bd = $_POST["day"];
 $by = $_POST["year"];
 $h_pw = password_hash($pw, PASSWORD_DEFAULT);
-$tquery = "select * from users where users.email = $em";
+$tquery = "select * from users where users.email = '$em'";
 $tresult = pg_query($tquery);
 $numrows = pg_num_rows($tresult);
 if ($numrows == 0)
 {
 	$query = "INSERT INTO users VALUES ('$fn', '$ln', '$em', '$h_pw', '$ad', '$ct', '$st', '$zc', '$bd', '$bm', '$by')";
 	$result = pg_query($query);
-	header('Location: /ECT/success.html');
-}
-else 
-{
-	header('Location: /ECT/failure.html');
-}
-//send email upon signup
-require 'PHPMailerAutoload.php';
+	//send email upon signup
 $mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->SMTPAuth = true;
 $mail->setFrom('thePersistentCookies@gmail.com', 'Crazy Cookies');
 $mail->Username = 'thePersistentCookies@gmail.com';
 $mail->Password = 'Uvaistheb3st!';
@@ -43,6 +48,13 @@ if(!$mail->send()) {
 } else {
   echo 'Message has been sent.';
 }
+	header('Location: /ECT/success.html');
+}
+else 
+{
+	header('Location: /ECT/failure.html');
+}
+
 ?>
 </body>
 </html>
