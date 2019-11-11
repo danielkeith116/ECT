15e9a1df2115f0ba99f0dc48d696a2e1e896bad1
danielkeith116 <danielkeith116@users.noreply.github.com>
2019-11-10 23:34:20 -0500

    sad moran

--- ECT/XAview.php
+++ ECT/XAview.php
@@ -14,7 +14,8 @@ if (!isset($_SESSION['email']))
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="/ECT/css/custommoran.css">
+    <link rel="stylesheet" type="text/css" href="/ECT/css/customsignup.css">
+    <link rel="stylesheet" type="text/css" href="/ECT/css/customstore.css">
     
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Dancing+Script&subset=latin,latin-ext" rel="stylesheet" type="text/css">
